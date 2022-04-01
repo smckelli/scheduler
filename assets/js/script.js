93995f6f3ee998a4202e0206f4f976220700d430
smckelli <smckelli@gmail.com>
2022-03-31 21:54:46 -0500

    created list of hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 
-var currentDay = moment().format('LLLL');
 
-$("#currentDay").text(currentDay);
+
+var updateTime = function() {
+
+document.getElementById("currentDay").innerHTML=moment().format('MMMM Do YYYY, h:mm:ss a');
+};
+setInterval(updateTime, 1000);
+
+
+
 
 
